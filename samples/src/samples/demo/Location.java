9f365d885684500f1b19dfb1203b4a73cbc212df
mustcoding <mahmormust@gmail.com>
2023-05-08 23:08:16 +0800

    commit location java

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,25 @@
+package samples.demo;
+
+public class Location {
+	
+	private int houseNumber;
+	private String Address;
+	
+	public int getHouseNumber() {
+		return houseNumber;
+	}
+	
+	public void setHouseNumber(int houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+	
+	public String getAddress() {
+		return Address;
+	}
+	
+	public void setAddress(String address) {
+		Address = address;
+	}
+	
+
+}
