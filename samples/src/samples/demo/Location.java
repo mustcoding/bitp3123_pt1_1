9297aaaf9404ebf890e86cf4d09c2e6930632a58
mustcoding <mahmormust@gmail.com>
2023-05-08 23:11:12 +0800

    latest location java

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -2,7 +2,7 @@ package samples.demo;
 
 public class Location {
 	
-	private int houseNumber;
+	
 	private String Address;
 	private int locationId;
 	
@@ -12,14 +12,6 @@ public class Location {
 
 	public void setLocationId(int locationId) {
 		this.locationId = locationId;
-	}
-
-	public int getHouseNumber() {
-		return houseNumber;
-	}
-	
-	public void setHouseNumber(int houseNumber) {
-		this.houseNumber = houseNumber;
 	}
 	
 	public String getAddress() {
